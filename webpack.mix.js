9d8b52df2aade1557b4232e6fbfea7ad6aa7d770
tyrants666 <akashyadav.iiita@gmail.com>
2021-04-07 10:05:15 +0545

    Laravel UI installed & Bootstrap scafolding done

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,23 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+
+mix.sass('resources/sass/app.scss', 'public/css/app.css')
+        .styles([
+            'public/css/app.css',
+            // 'public/css/jquery-select7.css',
+            'public/css/animate.css'
+        ], 'public/css/all.css');
+    //    let mix = require('laravel-mix');
+    
+    //    require('laravel-mix-purgecss');
+    
+   mix.js('resources/js/app.js', 'public/dist/app.js')
+       .sass('src/sass/app.scss', 'public/dist/app.css')
+       .purgeCss({
+           enabled: mix.inProduction(),
+           folders: ['src', 'templates'],
+           extensions: ['twig', 'html', 'js', 'php', 'vue'],
+       })
+       .setPublicPath('public');    
